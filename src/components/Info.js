eab83c30a84c457c7abf987f6b47e3593eb670a2
JoyAnneW <joyawilliams@gmail.com>
2022-01-14 10:47:26 +0100

    completed styling of Info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,29 +1,26 @@
 import React from "react";
-import joy from "../images/joy.jpeg";
+import laura from "../images/laura.jpg";
 
 export default function Info() {
 	return (
 		<div className="info">
-			<img className="info__img" src={joy} alt="Joy Williams" width="317px" />
+			<img className="info__img" src={laura} alt="Laura Smith" />
 
 			<div className="info__block--text">
-				<h3 className="info__block--name">Joy Williams</h3>
-				<p className="info__block--job">Full Stack Development Student</p>
+				<h1 className="info__block--name">Laura Smith</h1>
+				<h3 className="info__block--job">Frontend Developer</h3>
 				<a className="info--website" href="/">
-					joywilliams.website
+					laurasmith.website
 				</a>
 			</div>
 
 			<div className="info__block--btns">
-				<button className="email-btn">
-					<a href="mailto:joyawilliams@gmail.com">ICON Email</a>
+				<button className="btn email-btn">
+					<a href="mailto:laura@afh83jknl.com"> Email</a>
 				</button>
-				<button className="linkedin-btn">
-					<a
-						href="https://www.linkedin.com/in/joy-williams-8589a7176/"
-						target_blank
-					>
-						ICON LinkedIn
+				<button className="btn linkedin-btn">
+					<a href="https://www.linkedin.com" target_blank>
+						LinkedIn
 					</a>
 				</button>
 			</div>
