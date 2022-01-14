f8be510d3c4a4e855c4cdfc88c5da02ba2cde670
JoyAnneW <joyawilliams@gmail.com>
2022-01-14 15:52:55 +0100

    completed styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from "react";
+import twitter from "../images/Twitter.png";
+import facebook from "../images/Facebook.png";
+import instagram from "../images/Instagram.png";
+import github from "../images/GitHub.png";
 
 export default function Footer() {
 	return (
 		<footer className="footer">
-			<div className="footer__icons">Footer: 4 icons to go here</div>
+			<div className="footer__icons">
+				<a href="www.twitter.com">
+					<img src={twitter} alt="twitter icon" />
+				</a>
+				<a href="www.facebook.com">
+					<img src={facebook} alt="facebook icon" />
+				</a>
+				<a href="www.instagram.com">
+					<img src={instagram} alt="instagram icon" />
+				</a>
+				<a href="www.github.com">
+					<img src={github} alt="github icon" />
+				</a>
+			</div>
 		</footer>
 	);
 }
